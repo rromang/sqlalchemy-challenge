379b61d8c065c8a4c770265b441425f2d84efac6
rromang <80008461+rromang@users.noreply.github.com>
2021-05-20 20:40:33 -0700

    testing app file

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
 from flask import Flask, jsonify
 
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+import pandas as pd
+import datetime as dt
+
+# Flask Setup
+
+app = Flask(__name__)
+
+# Flask Routes
+
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to your Hawai'i Weather Exploration API!"
+        f"Available Routes:"
+        f'/api/v1.0/precipitation'
+    )
+
